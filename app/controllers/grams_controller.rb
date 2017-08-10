0cf89b9b72882c700d484d7d6800a20cdb066b73
Kevin Kawai <kevin.fredricks@gmail.com>
2017-08-10 16:22:41 +0000

    Made only the user show created be able to edit,update,destroy gram

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,5 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:create]
+  before_action :authenticate_user!, only: [:new,:create,:edit,:update,:destroy]
 
   def index
   end
@@ -25,11 +25,17 @@ class GramsController < ApplicationController
   def edit
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    if @gram.user != current_user
+      return render plain: 'Forbidden :<', status: :forbidden
+    end
   end
 
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    if @gram.user != current_user
+      return render_not_found(:forbidden) if @gram.user != current_user
+    end
 
     @gram.update_attributes(gram_params)
 
@@ -42,8 +48,10 @@ class GramsController < ApplicationController
 
   def destroy
     @gram = Gram.find_by_id(params[:id])
-
     return render_not_found if @gram.blank?
+    if @gram.user != current_user
+      return render_not_found(:forbidden) if @gram.user != current_user
+    end
 
     @gram.destroy
     redirect_to root_path
@@ -55,7 +63,7 @@ class GramsController < ApplicationController
     params.require(:gram).permit(:message)
   end
 
-  def render_not_found
-    render plain: 'Not Found :<', status: :not_found
+  def render_not_found(status=:not_found)
+    render plain: "#{status.to_s.titleize} :<", status: status
   end
 end
